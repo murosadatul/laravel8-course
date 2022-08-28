9dd2f5e19b1cd5bb3d7523556453c9c8855bf74e
Murosadatul <murosadatul@gmail.com>
2022-08-28 10:41:25 +0700

    add fortify

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -2,15 +2,19 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use App\Actions\Fortify\CreateNewUser;
 use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Fortify;
+use League\CommonMark\Extension\Attributes\Node\Attributes;
+use Illuminate\Contracts\Auth\Authenticatable;
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -44,6 +48,18 @@ class FortifyServiceProvider extends ServiceProvider
 
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        });
+
+        Fortify::loginView(function(){
+            return view('auth.login');
+        });
+
+        Fortify::registerView(function(){
+            return view('auth.register');
+        });
+
+        Fortify::requestPasswordResetLinkView(function(){
+            return view('auth.forgot-password');
         });
     }
 }
